4cc4d918530cbb36911caaf90e057852aa8f4c67
Snesnopic <snesnopic@gmail.com>
2024-06-19 15:53:37 +0200

    Initial Commit

---
+++ WristGolf Watch App/WristGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristGolfApp.swift
+//  WristGolf Watch App
+//
+//  Created by Giuseppe Francione on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WristGolf_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WristGolf/WristGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristGolfApp.swift
+//  WristGolf
+//
+//  Created by Giuseppe Francione on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WristGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
